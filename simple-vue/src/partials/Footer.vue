3dacfc6681249fbb26051ae6de7e72a9df28e883
lucy-xfl <86309711+lucy-xfl@users.noreply.github.com>
2024-03-15 16:17:53 -0700

    update header and footer

--- simple-vue/src/partials/Footer.vue
+++ simple-vue/src/partials/Footer.vue
@@ -1,112 +1,184 @@
 <template>
   <footer>
     <div class="max-w-6xl mx-auto px-4 sm:px-6">
-
       <!-- Top area: Blocks -->
-      <div class="grid sm:grid-cols-12 gap-8 py-8 md:py-12 border-t border-gray-200">
-
+      <div
+        class="grid sm:grid-cols-12 gap-8 py-8 md:py-12 border-t border-gray-200"
+      >
         <!-- 1st block -->
-        <div class="sm:col-span-12 lg:col-span-3">
+        <div class="sm:col-span-12 lg:col-span-2">
           <div class="mb-2">
             <!-- Logo -->
             <router-link to="/" class="inline-block" aria-label="Cruip">
-              <svg class="w-8 h-8" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
-                <defs>
-                  <radialGradient cx="21.152%" cy="86.063%" fx="21.152%" fy="86.063%" r="79.941%" id="footer-logo">
-                    <stop stop-color="#4FD1C5" offset="0%" />
-                    <stop stop-color="#81E6D9" offset="25.871%" />
-                    <stop stop-color="#338CF5" offset="100%" />
-                  </radialGradient>
-                </defs>
-                <rect width="32" height="32" rx="16" fill="url(#footer-logo)" fill-rule="nonzero" />
-              </svg>
+              <router-link to="/" class="flex items-center" aria-label="Cruip">
+                <img src="icon.jpg" alt="Icon" class="w-12 h-12 mr-2" />
+                <h3>思益清源</h3>
+              </router-link>
             </router-link>
           </div>
           <div class="text-sm text-gray-600">
-            <router-link to="#" class="text-gray-600 hover:text-gray-900 hover:underline transition duration-150 ease-in-out">Terms</router-link> · <router-link to="#" class="text-gray-600 hover:text-gray-900 hover:underline transition duration-150 ease-in-out">Privacy Policy</router-link>
+            <router-link
+              to="#"
+              class="text-gray-600 hover:text-gray-900 hover:underline transition duration-150 ease-in-out"
+              >Terms</router-link
+            >
+            ·
+            <router-link
+              to="#"
+              class="text-gray-600 hover:text-gray-900 hover:underline transition duration-150 ease-in-out"
+              >Privacy Policy</router-link
+            >
           </div>
         </div>
 
         <!-- 2nd block -->
-        <div class="sm:col-span-6 md:col-span-3 lg:col-span-2">
-          <h6 class="text-gray-800 font-medium mb-2">Products</h6>
+        <div class="sm:col-span-6 md:col-span-2 lg:col-span-2">
+          <h6 class="text-gray-800 font-medium mb-2">关于我们</h6>
           <ul class="text-sm">
             <li class="mb-2">
-              <router-link to="#" class="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out">Web Studio</router-link>
+              <router-link
+                to="#"
+                class="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out"
+                >公司概况</router-link
+              >
             </li>
             <li class="mb-2">
-              <router-link to="#" class="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out">DynamicBox Flex</router-link>
+              <router-link
+                to="#"
+                class="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out"
+                >企业文化</router-link
+              >
             </li>
             <li class="mb-2">
-              <router-link to="#" class="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out">Programming Forms</router-link>
-            </li>
-            <li class="mb-2">
-              <router-link to="#" class="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out">Integrations</router-link>
-            </li>
-            <li class="mb-2">
-              <router-link to="#" class="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out">Command-line</router-link>
+              <router-link
+                to="#"
+                class="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out"
+                >人才队伍</router-link
+              >
             </li>
           </ul>
         </div>
 
         <!-- 3rd block -->
-        <div class="sm:col-span-6 md:col-span-3 lg:col-span-2">
-          <h6 class="text-gray-800 font-medium mb-2">Resources</h6>
+        <div class="sm:col-span-6 md:col-span-2 lg:col-span-2">
+          <h6 class="text-gray-800 font-medium mb-2">业务范围</h6>
           <ul class="text-sm">
             <li class="mb-2">
-              <router-link to="#" class="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out">Documentation</router-link>
+              <router-link
+                to="#"
+                class="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out"
+                >智慧水利</router-link
+              >
             </li>
             <li class="mb-2">
-              <router-link to="#" class="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out">Tutorials & Guides</router-link>
+              <router-link
+                to="#"
+                class="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out"
+                >智慧水质</router-link
+              >
             </li>
             <li class="mb-2">
-              <router-link to="#" class="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out">Blog</router-link>
+              <router-link
+                to="#"
+                class="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out"
+                >智慧生态</router-link
+              >
             </li>
             <li class="mb-2">
-              <router-link to="#" class="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out">Support Center</router-link>
-            </li>
-            <li class="mb-2">
-              <router-link to="#" class="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out">Partners</router-link>
+              <router-link
+                to="#"
+                class="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out"
+                >数字孪生</router-link
+              >
             </li>
           </ul>
         </div>
 
         <!-- 4th block -->
-        <div class="sm:col-span-6 md:col-span-3 lg:col-span-2">
-          <h6 class="text-gray-800 font-medium mb-2">Company</h6>
+        <div class="sm:col-span-6 md:col-span-2 lg:col-span-2">
+          <h6 class="text-gray-800 font-medium mb-2">公司动态</h6>
           <ul class="text-sm">
             <li class="mb-2">
-              <router-link to="#" class="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out">Home</router-link>
+              <router-link
+                to="#"
+                class="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out"
+                >项目案例</router-link
+              >
             </li>
             <li class="mb-2">
-              <router-link to="#" class="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out">About us</router-link>
+              <router-link
+                to="#"
+                class="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out"
+                >实习招聘</router-link
+              >
             </li>
             <li class="mb-2">
-              <router-link to="#" class="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out">Company values</router-link>
+              <router-link
+                to="#"
+                class="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out"
+                >学术会议</router-link
+              >
             </li>
             <li class="mb-2">
-              <router-link to="#" class="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out">Pricing</router-link>
-            </li>
-            <li class="mb-2">
-              <router-link to="#" class="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out">Privacy Policy</router-link>
+              <router-link
+                to="#"
+                class="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out"
+                >相关培训</router-link
+              >
             </li>
           </ul>
         </div>
 
         <!-- 5th block -->
-        <div class="sm:col-span-6 md:col-span-3 lg:col-span-3">
-          <h6 class="text-gray-800 font-medium mb-2">Subscribe</h6>
-          <p class="text-sm text-gray-600 mb-4">Get the latest news and articles to your inbox every month.</p>
+        <div class="sm:col-span-6 md:col-span-2 lg:col-span-2">
+          <h6 class="text-gray-800 font-medium mb-2">软件下载</h6>
+          <ul class="text-sm">
+            <li class="mb-2">
+              <router-link
+                to="#"
+                class="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out"
+                >下载链接</router-link
+              >
+            </li>
+          </ul>
+        </div>
+
+        <!-- 6th block -->
+        <div class="sm:col-span-6 md:col-span-2 lg:col-span-2">
+          <h6 class="text-gray-800 font-medium mb-2">联系我们</h6>
+          <p class="text-sm text-gray-600 mb-4">获取每月最新资讯</p>
           <form>
             <div class="flex flex-wrap mb-4">
               <div class="w-full">
-                <label class="block text-sm sr-only" for="newsletter">Email</label>
+                <label class="block text-sm sr-only" for="newsletter"
+                  >Email</label
+                >
                 <div class="relative flex items-center max-w-xs">
-                  <input id="newsletter" type="email" class="form-input w-full text-gray-800 px-3 py-2 pr-12 text-sm" placeholder="Your email" required />
-                  <button type="submit" class="absolute inset-0 left-auto" aria-label="Subscribe">
-                    <span class="absolute inset-0 right-auto w-px -ml-px my-2 bg-gray-300" aria-hidden="true"></span>
-                    <svg class="w-3 h-3 fill-current text-blue-600 mx-3 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
-                      <path d="M11.707 5.293L7 .586 5.586 2l3 3H0v2h8.586l-3 3L7 11.414l4.707-4.707a1 1 0 000-1.414z" fill-rule="nonzero" />
+                  <input
+                    id="newsletter"
+                    type="email"
+                    class="form-input w-full text-gray-800 px-3 py-2 pr-12 text-sm"
+                    placeholder="您的邮箱"
+                    required
+                  />
+                  <button
+                    type="submit"
+                    class="absolute inset-0 left-auto"
+                    aria-label="Subscribe"
+                  >
+                    <span
+                      class="absolute inset-0 right-auto w-px -ml-px my-2 bg-gray-300"
+                      aria-hidden="true"
+                    ></span>
+                    <svg
+                      class="w-3 h-3 fill-current text-blue-600 mx-3 shrink-0"
+                      viewBox="0 0 12 12"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M11.707 5.293L7 .586 5.586 2l3 3H0v2h8.586l-3 3L7 11.414l4.707-4.707a1 1 0 000-1.414z"
+                        fill-rule="nonzero"
+                      />
                     </svg>
                   </button>
                 </div>
@@ -116,42 +188,72 @@
             </div>
           </form>
         </div>
-
       </div>
 
       <!-- Bottom area -->
-      <div class="md:flex md:items-center md:justify-between py-4 md:py-8 border-t border-gray-200">
-
+      <div
+        class="md:flex md:items-center md:justify-between py-4 md:py-8 border-t border-gray-200"
+      >
         <!-- Social links -->
         <ul class="flex mb-4 md:order-1 md:ml-4 md:mb-0">
           <li>
-            <router-link to="#" class="flex justify-center items-center text-gray-600 hover:text-gray-900 bg-white hover:bg-white-100 rounded-full shadow transition duration-150 ease-in-out" aria-label="Twitter">
-              <svg class="w-8 h-8 fill-current" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
-                <path d="m13.063 9 3.495 4.475L20.601 9h2.454l-5.359 5.931L24 23h-4.938l-3.866-4.893L10.771 23H8.316l5.735-6.342L8 9h5.063Zm-.74 1.347h-1.457l8.875 11.232h1.36l-8.778-11.232Z" />
+            <router-link
+              to="#"
+              class="flex justify-center items-center text-gray-600 hover:text-gray-900 bg-white hover:bg-white-100 rounded-full shadow transition duration-150 ease-in-out"
+              aria-label="Twitter"
+            >
+              <svg
+                class="w-8 h-8 fill-current"
+                viewBox="0 0 32 32"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="m13.063 9 3.495 4.475L20.601 9h2.454l-5.359 5.931L24 23h-4.938l-3.866-4.893L10.771 23H8.316l5.735-6.342L8 9h5.063Zm-.74 1.347h-1.457l8.875 11.232h1.36l-8.778-11.232Z"
+                />
               </svg>
             </router-link>
           </li>
           <li class="ml-4">
-            <router-link to="#" class="flex justify-center items-center text-gray-600 hover:text-gray-900 bg-white hover:bg-white-100 rounded-full shadow transition duration-150 ease-in-out" aria-label="Github">
-              <svg class="w-8 h-8 fill-current" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
-                <path d="M16 8.2c-4.4 0-8 3.6-8 8 0 3.5 2.3 6.5 5.5 7.6.4.1.5-.2.5-.4V22c-2.2.5-2.7-1-2.7-1-.4-.9-.9-1.2-.9-1.2-.7-.5.1-.5.1-.5.8.1 1.2.8 1.2.8.7 1.3 1.9.9 2.3.7.1-.5.3-.9.5-1.1-1.8-.2-3.6-.9-3.6-4 0-.9.3-1.6.8-2.1-.1-.2-.4-1 .1-2.1 0 0 .7-.2 2.2.8.6-.2 1.3-.3 2-.3s1.4.1 2 .3c1.5-1 2.2-.8 2.2-.8.4 1.1.2 1.9.1 2.1.5.6.8 1.3.8 2.1 0 3.1-1.9 3.7-3.7 3.9.3.4.6.9.6 1.6v2.2c0 .2.1.5.6.4 3.2-1.1 5.5-4.1 5.5-7.6-.1-4.4-3.7-8-8.1-8z" />
+            <router-link
+              to="#"
+              class="flex justify-center items-center text-gray-600 hover:text-gray-900 bg-white hover:bg-white-100 rounded-full shadow transition duration-150 ease-in-out"
+              aria-label="Github"
+            >
+              <svg
+                class="w-8 h-8 fill-current"
+                viewBox="0 0 32 32"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M16 8.2c-4.4 0-8 3.6-8 8 0 3.5 2.3 6.5 5.5 7.6.4.1.5-.2.5-.4V22c-2.2.5-2.7-1-2.7-1-.4-.9-.9-1.2-.9-1.2-.7-.5.1-.5.1-.5.8.1 1.2.8 1.2.8.7 1.3 1.9.9 2.3.7.1-.5.3-.9.5-1.1-1.8-.2-3.6-.9-3.6-4 0-.9.3-1.6.8-2.1-.1-.2-.4-1 .1-2.1 0 0 .7-.2 2.2.8.6-.2 1.3-.3 2-.3s1.4.1 2 .3c1.5-1 2.2-.8 2.2-.8.4 1.1.2 1.9.1 2.1.5.6.8 1.3.8 2.1 0 3.1-1.9 3.7-3.7 3.9.3.4.6.9.6 1.6v2.2c0 .2.1.5.6.4 3.2-1.1 5.5-4.1 5.5-7.6-.1-4.4-3.7-8-8.1-8z"
+                />
               </svg>
             </router-link>
           </li>
           <li class="ml-4">
-            <router-link to="#" class="flex justify-center items-center text-gray-600 hover:text-gray-900 bg-white hover:bg-white-100 rounded-full shadow transition duration-150 ease-in-out" aria-label="Facebook">
-              <svg class="w-8 h-8 fill-current" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
-                <path d="M14.023 24L14 17h-3v-3h3v-2c0-2.7 1.672-4 4.08-4 1.153 0 2.144.086 2.433.124v2.821h-1.67c-1.31 0-1.563.623-1.563 1.536V14H21l-1 3h-2.72v7h-3.257z" />
+            <router-link
+              to="#"
+              class="flex justify-center items-center text-gray-600 hover:text-gray-900 bg-white hover:bg-white-100 rounded-full shadow transition duration-150 ease-in-out"
+              aria-label="Facebook"
+            >
+              <svg
+                class="w-8 h-8 fill-current"
+                viewBox="0 0 32 32"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M14.023 24L14 17h-3v-3h3v-2c0-2.7 1.672-4 4.08-4 1.153 0 2.144.086 2.433.124v2.821h-1.67c-1.31 0-1.563.623-1.563 1.536V14H21l-1 3h-2.72v7h-3.257z"
+                />
               </svg>
             </router-link>
           </li>
         </ul>
 
         <!-- Copyrights note -->
-        <div class="text-sm text-gray-600 mr-4">&copy; Cruip.com. All rights reserved.</div>
-
+        <div class="text-sm text-gray-600 mr-4">
+          &copy; EcoSci.com. All rights reserved.
+        </div>
       </div>
-
     </div>
   </footer>
 </template>
@@ -159,5 +261,5 @@
 <script>
 export default {
   name: "Footer",
-}
+};
 </script>
--- simple-vue/src/partials/Header.vue
+++ simple-vue/src/partials/Header.vue
@@ -8,34 +8,9 @@
         <!-- Site branding -->
         <div class="shrink-0 mr-4">
           <!-- Logo -->
-          <router-link to="/" class="block" aria-label="Cruip">
-            <svg
-              class="w-8 h-8"
-              viewBox="0 0 32 32"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <defs>
-                <radialGradient
-                  cx="21.152%"
-                  cy="86.063%"
-                  fx="21.152%"
-                  fy="86.063%"
-                  r="79.941%"
-                  id="header-logo"
-                >
-                  <stop stop-color="#4FD1C5" offset="0%" />
-                  <stop stop-color="#81E6D9" offset="25.871%" />
-                  <stop stop-color="#338CF5" offset="100%" />
-                </radialGradient>
-              </defs>
-              <rect
-                width="32"
-                height="32"
-                rx="16"
-                fill="url(#header-logo)"
-                fill-rule="nonzero"
-              />
-            </svg>
+          <router-link to="/" class="flex items-center" aria-label="Cruip">
+            <img src="icon.jpg" alt="Icon" class="w-12 h-12 mr-2" />
+            <h3>思益清源</h3>
           </router-link>
         </div>
 
@@ -45,92 +20,47 @@
           <ul class="flex grow justify-end flex-wrap items-center">
             <li>
               <router-link
-                to="/"
+                to="/about"
                 class="text-gray-600 hover:text-gray-900 px-3 lg:px-5 py-2 flex items-center transition duration-150 ease-in-out"
-                >Home</router-link
-              >
-            </li>
-            <li>
-              <router-link
-                to="/pricing"
-                class="text-gray-600 hover:text-gray-900 px-3 lg:px-5 py-2 flex items-center transition duration-150 ease-in-out"
-                >Pricing</router-link
+                >关于我们</router-link
               >
             </li>
             <li>
               <router-link
                 to="/about"
                 class="text-gray-600 hover:text-gray-900 px-3 lg:px-5 py-2 flex items-center transition duration-150 ease-in-out"
-                >About us</router-link
+                >业务范围</router-link
               >
             </li>
             <li>
               <router-link
-                to="/tutorials"
+                to="/about"
                 class="text-gray-600 hover:text-gray-900 px-3 lg:px-5 py-2 flex items-center transition duration-150 ease-in-out"
-                >Tutorials</router-link
+                >公司动态</router-link
               >
             </li>
             <li>
               <router-link
                 to="/blog"
                 class="text-gray-600 hover:text-gray-900 px-3 lg:px-5 py-2 flex items-center transition duration-150 ease-in-out"
-                >Blog</router-link
-              >
-            </li>
-            <!-- 1st level: hover -->
-            <Dropdown title="Resources">
-              <li>
-                <router-link
-                  to="/documentation"
-                  class="font-medium text-sm text-gray-600 hover:text-gray-900 flex py-2 px-5 leading-tight"
-                  >Documentation</router-link
-                >
-              </li>
-              <li>
-                <router-link
-                  to="/support"
-                  class="font-medium text-sm text-gray-600 hover:text-gray-900 flex py-2 px-5 leading-tight"
-                  >Support center</router-link
-                >
-              </li>
-              <li>
-                <router-link
-                  to="/404"
-                  class="font-medium text-sm text-gray-600 hover:text-gray-900 flex py-2 px-5 leading-tight"
-                  >404</router-link
-                >
-              </li>
-            </Dropdown>
-          </ul>
-
-          <!-- Desktop sign in links -->
-          <ul class="flex grow justify-end flex-wrap items-center">
-            <li>
-              <router-link
-                to="/signin"
-                class="font-medium text-gray-600 hover:text-gray-900 px-5 py-3 flex items-center transition duration-150 ease-in-out"
-                >Sign in</router-link
+                >项目案例</router-link
               >
             </li>
             <li>
               <router-link
-                to="/signup"
-                class="btn-sm text-gray-200 bg-gray-900 hover:bg-gray-800 ml-3"
+                to="/blog"
+                class="text-gray-600 hover:text-gray-900 px-3 lg:px-5 py-2 flex items-center transition duration-150 ease-in-out"
+                >联系我们</router-link
               >
-                <span>Sign up</span>
-                <svg
-                  class="w-3 h-3 fill-current text-gray-400 shrink-0 ml-2 -mr-1"
-                  viewBox="0 0 12 12"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M11.707 5.293L7 .586 5.586 2l3 3H0v2h8.586l-3 3L7 11.414l4.707-4.707a1 1 0 000-1.414z"
-                    fill-rule="nonzero"
-                  />
-                </svg>
-              </router-link>
             </li>
+            <li>
+              <router-link
+                to="/blog"
+                class="text-gray-600 hover:text-gray-900 px-3 lg:px-5 py-2 flex items-center transition duration-150 ease-in-out"
+                >软件下载</router-link
+              >
+            </li>
+            <!-- 后期加入中英文转换按钮 -->
           </ul>
         </nav>
 
@@ -175,85 +105,45 @@
               <ul class="px-5 py-2">
                 <li>
                   <router-link
-                    to="/pricing"
-                    class="flex text-gray-600 hover:text-gray-900 py-2"
-                    >Pricing</router-link
-                  >
-                </li>
-                <li>
-                  <router-link
                     to="/about"
                     class="flex text-gray-600 hover:text-gray-900 py-2"
-                    >About us</router-link
+                    >关于我们</router-link
                   >
                 </li>
                 <li>
                   <router-link
                     to="/tutorials"
                     class="flex text-gray-600 hover:text-gray-900 py-2"
-                    >Tutorials</router-link
+                    >业务范围</router-link
                   >
                 </li>
                 <li>
                   <router-link
                     to="/blog"
                     class="flex text-gray-600 hover:text-gray-900 py-2"
-                    >Blog</router-link
-                  >
-                </li>
-                <li class="py-2 my-2 border-t border-b border-gray-200">
-                  <span class="flex text-gray-600 hover:text-gray-900 py-2"
-                    >Resources</span
-                  >
-                  <ul class="pl-4">
-                    <li>
-                      <router-link
-                        to="/documentation"
-                        class="text-sm flex font-medium text-gray-600 hover:text-gray-900 py-2"
-                        >Documentation</router-link
-                      >
-                    </li>
-                    <li>
-                      <router-link
-                        to="/support"
-                        class="text-sm flex font-medium text-gray-600 hover:text-gray-900 py-2"
-                        >Support center</router-link
-                      >
-                    </li>
-                    <li>
-                      <router-link
-                        to="/404"
-                        class="text-sm flex font-medium text-gray-600 hover:text-gray-900 py-2"
-                        >404</router-link
-                      >
-                    </li>
-                  </ul>
-                </li>
-                <li>
-                  <router-link
-                    to="/signin"
-                    class="flex font-medium w-full text-gray-600 hover:text-gray-900 py-2 justify-center"
-                    >Sign in</router-link
+                    >公司动态</router-link
                   >
                 </li>
                 <li>
                   <router-link
-                    to="/signup"
-                    class="btn-sm text-gray-200 bg-gray-900 hover:bg-gray-800 w-full my-2"
+                    to="/blog"
+                    class="flex text-gray-600 hover:text-gray-900 py-2"
+                    >项目案例</router-link
                   >
-                    <span>Sign up</span>
-                    <svg
-                      class="w-3 h-3 fill-current text-gray-400 shrink-0 ml-2 -mr-1"
-                      viewBox="0 0 12 12"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        d="M11.707 5.293L7 .586 5.586 2l3 3H0v2h8.586l-3 3L7 11.414l4.707-4.707a1 1 0 000-1.414z"
-                        fill="#999"
-                        fill-rule="nonzero"
-                      />
-                    </svg>
-                  </router-link>
+                </li>
+                <li>
+                  <router-link
+                    to="/blog"
+                    class="flex text-gray-600 hover:text-gray-900 py-2"
+                    >联系我们</router-link
+                  >
+                </li>
+                <li>
+                  <router-link
+                    to="/blog"
+                    class="flex text-gray-600 hover:text-gray-900 py-2"
+                    >软件下载</router-link
+                  >
                 </li>
               </ul>
             </nav>
